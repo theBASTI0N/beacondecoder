165d6e157520213a65f8ef6995e76d75c23ce753
theBASTI0N <thebasti0ncode@gmail.com>
2020-07-30 12:23:34 +0800

    v0.5

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
-from distutils.core import setup
-setup(
+import setuptools
+
+setuptools.setup(
   name = 'beacondecoder',
   packages = ['beacondecoder'],
-  version = '0.4',  
+  version = '0.5.0',  
   license='MIT',
   description = 'A Python library for decoding certain types of Bluetooth LE Beacons',
   author = 'theBASTI0N',
   author_email = 'theBASTI0Ncode@gmail.com',
   url = 'https://github.com/theBASTI0N/beacon-decoder',
-  download_url = 'https://github.com/theBASTI0N/beacondecoder/archive/0.4.tar.gz',
+  download_url = 'https://github.com/theBASTI0N/beacondecoder/archive/0.5.tar.gz',
   keywords = ['BLE', 'decode', 'iot'],
   install_requires=[],
   classifiers=[
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
